b72dc8412dc48ae868c8bbd3fea24e487db596e1
Allan Maradiaga <alfomagos@gmail.com>
2016-02-18 13:18:42 -0600

    antes de remediar conflicto entre branches

--- model/persona.py
+++ model/persona.py
@@ -6,13 +6,8 @@ class Persona(osv.Model):
 	_name = "cein.persona"
 	_rac_name='name'
 	_columns = {
-<<<<<<< HEAD
 		'name': fields.char(string="Apellidos y Nombres",	 size=256, required=True,help="Nombre sin acentos opcionalmente para optimizar busquedas"),
 		'displayname': fields.char(string="Apellidos y Nombres",	 size=256, required=True,help="Nombre con acentos a mostrar"),
-=======
-		'name': fields.char(string="Identidad",	 size=256, required=True,help="Nombre sin acentos opcionalmente para optimizar busquedas"),
-		'displayname': fields.char(string="Nombres y Apellidos",	 size=256, required=True,help="Nombre con acentos a mostrar"),
->>>>>>> 8ef01ec620a007d1b037eef5a39ae0af4f07a46d
 		'nacionalidad_id': fields.many2one('cein.nacionalidad','Nacionalidad'),
 		'escolaridad_id': fields.many2one('cein.escolaridad','escolaridad'),
 		'estadocivil_id': fields.many2one('cein.estadocivil','estadocivil'),
